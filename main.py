04bcc5bc90745c34279bc8216a97a3d3ac67821b
migueleduardobravo-debug <migueleduardobravo@gmail.com>
2026-04-19 12:51:20 +0200

    FIX: Desactivación del modo Debug en entorno de desarrollo para prevenir RCE (CWE-489)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from server import app
 from routes import auth, companies, companies_admin, users_admin, profile
 
-if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+if __name__ == '__main__':
+    # Cambiamos debug a False para cumplir con estándares de producción
+    app.run(debug=False, port=5000)
